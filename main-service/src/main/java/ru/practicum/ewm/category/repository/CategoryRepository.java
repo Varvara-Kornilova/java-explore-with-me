e3ddc9d4bd7a3268bcc6adb8046187e4aab70eb1
Varvara-Kornilova <vrvrmoren@yandex.ru>
2026-06-16 14:15:21 +0300

    # использовать оператор EXISTS вместо COUNT

--- main-service/src/main/java/ru/practicum/ewm/category/repository/CategoryRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/category/repository/CategoryRepository.java
@@ -2,10 +2,11 @@ package ru.practicum.ewm.category.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.ewm.category.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query("SELECT COUNT(e) > 0 FROM Event e WHERE e.category.id = :categoryId")
-    boolean existsByCategoryId(Long categoryId);
+    @Query("SELECT EXISTS (SELECT 1 FROM Event e WHERE e.category.id = :categoryId)")
+    boolean existsByCategoryId(@Param("categoryId") Long categoryId);
 }
